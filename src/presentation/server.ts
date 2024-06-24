95d2cae3d58724e6b6771ffce60a3572b0d804b5
Samuel <toboncs@gmail.com>
2024-06-23 20:48:29 -0500

    Thrid commit

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,41 +1,56 @@
-import express from 'express';
+import express, { Router } from 'express';
 import path from 'path';
 
 interface Options {
     port: number,
+    routes: Router,
     public_path?: string
 }
 
-export class Server{
+export class Server {
 
     private app = express();
     private readonly port: number;
     private readonly publicPath: string;
+    private readonly routes: Router;
 
-    constructor(options: Options){
+
+    constructor(options: Options) {
         const {
             port,
+            routes,
             public_path = 'public'
         } = options;
         this.port = port;
+        this.routes = routes
         this.publicPath = public_path
     }
-    async start(){
-        console.log("Server running... ");
 
-            // Middlewares
 
-    //Public folder
+    async start() {
 
-    this.app.use( express.static(this.publicPath));
+        // Middlewares
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: true }));
 
-    this.app.get('*', (req,res) =>{
-       const indexPath = path.join(__dirname, `../../${this.publicPath}/index.html`);
-       res.sendFile(indexPath);
-       return;
-    })
 
-        this.app.listen(this.port, ()=>{
+        //Public folder
+        this.app.use(express.static(this.publicPath));
+
+        //* Routes
+        this.app.use(this.routes);
+
+
+        // SPA
+        this.app.get('*', (req, res) => {
+            const indexPath = path.join(__dirname, `../../${this.publicPath}/index.html`);
+            res.sendFile(indexPath);
+            return;
+        })
+
+
+
+        this.app.listen(this.port, () => {
             console.log(`Server is running on port ${3000}`);
         });
     }
